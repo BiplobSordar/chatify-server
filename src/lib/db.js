32732f6bfc8dc7f562982357bdcaa6c7e71222ee
unknown <biplob.code.bd@gmail.com>
2025-12-30 14:29:51 +0600

    fix mongodb connection

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,15 +1,18 @@
+
+
 import mongoose from "mongoose";
-import { ENV } from "./env.js";
 
-export const connectDB = async () => {
+const connectDB = async () => {
   try {
-    const { MONGO_URI } = ENV;
-    if (!MONGO_URI) throw new Error("MONGO_URI is not set");
-
-    const conn = await mongoose.connect(ENV.MONGO_URI);
-    console.log("MONGODB CONNECTED:", conn.connection.host);
-  } catch (error) {
-    console.error("Error connection to MONGODB:", error);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
     process.exit(1); 
   }
 };
+
+export default connectDB;
