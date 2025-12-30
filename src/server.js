ccf300ae3cd192b012b85d158a078baa6a83266d
unknown <biplob.code.bd@gmail.com>
2025-12-30 14:22:48 +0600

    Fix the cors error

--- src/server.js
+++ src/server.js
@@ -14,20 +14,36 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json({ limit: "5mb" }));
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+const allowedOrigins = [
+  process.env.CLIENT_URL?.replace(/\/$/, ""), 
+];
+
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+     
+      if (!origin) return callback(null, true);
+
+    
+      const cleanOrigin = origin.replace(/\/$/, "");
+
+      if (allowedOrigins.includes(cleanOrigin) || cleanOrigin.endsWith(".vercel.app")) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 
-// if (ENV.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-//   app.get("*", (_, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
 
 app.get("/", (req, res) => {
   res.send("Chatify Server is running ");
