e193ee99925e2a69b8d2f4940bf517bc36241dda
unknown <biplob.code.bd@gmail.com>
2025-12-30 14:37:56 +0600

    Fixing again

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,16 +1,15 @@
-
 import mongoose from "mongoose";
+import { ENV } from "./env.js";
 
- export const connectDB = async () => {
+export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected");
-  } catch (err) {
-    console.error("MongoDB connection failed:", err);
-    process.exit(1); // optional: crash if DB connection fails
+    const { MONGO_URI } = ENV;
+    if (!MONGO_URI) throw new Error("MONGO_URI is not set");
+
+    const conn = await mongoose.connect(ENV.MONGO_URI);
+    console.log("MONGODB CONNECTED:", conn.connection.host);
+  } catch (error) {
+    console.error("Error connection to MONGODB:", error);
+    process.exit(1); // 1 status code means fail, 0 means success
   }
 };
-
